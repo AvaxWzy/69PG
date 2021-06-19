e740522b89cf792bd456ef8aaf0cf17e3c91544b
Avax <85783328+AvaxWzy@users.noreply.github.com>
2021-06-20 00:33:13 +0600

    Discord.js extended client

---
+++ src/class/Client.js
@@ -0,0 +1,30 @@
+// Copyright 2021 AvaxWzy
+
+/*Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+class Client extends require("discord.js").Client {
+    constructor() {
+        super({
+            messageCacheLifetime: 10e20,
+            disableMentions: "everyone",
+            ws: {
+                intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]
+            }
+        });
+
+        this.commands = new (require("discord.js").Collection)();
+        this.aliases = new (require("discord.js").Collection)();
+    };
+};
+
+module.exports = Client;
