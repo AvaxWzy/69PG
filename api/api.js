3604f0c57662b424b7f0a04bca12b03b7bfe65ff
Avax <85783328+AvaxWzy@users.noreply.github.com>
2021-06-25 14:28:37 +0600

    Port changed to 8000

--- api/api.js
+++ api/api.js
@@ -7,6 +7,6 @@ app.set("view engine", "ejs");
 
 app.use(require("./routers/home"));
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log("Api started");
 })
