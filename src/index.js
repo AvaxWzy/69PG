08181ffb8725a406fac6bf6dee58a618da3f75b6
Avax <85783328+AvaxWzy@users.noreply.github.com>
2021-06-25 14:18:04 +0600

    Index updated

--- src/index.js
+++ src/index.js
@@ -19,6 +19,6 @@ const client = new Client();
     require(`./handlers/${x}`)(client);
 });
 
-client.database(require("./config/bot.json").mongourl);
+client.database(process.env.mongourl || require("./config/bot.json").mongourl);
 
 client.login(process.env.token || require("./config/bot.json").token);
